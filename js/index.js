a5da7fe58d8d4b27b33ba15ddd4a02539d4984a2
skooger <adamjskooglambda@gmail.com>
2019-12-02 14:24:52 -0800

    Completed the base project. About to start on stretch goals

--- js/index.js
+++ js/index.js
@@ -40,3 +40,77 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+let nav = document.querySelectorAll('a')
+let i = 0
+for(let link of nav ){
+  link.textContent = siteContent["nav"]["nav-item-"+i]
+  i++
+}
+
+let h1 = document.querySelector('h1')
+h1.textContent = siteContent["cta"]["h1"]
+
+let button = document.querySelector('button')
+button.textContent = siteContent["cta"]["button"]
+
+let cta = document.getElementById('cta-img')
+cta.setAttribute('src', siteContent["cta"]["img-src"])
+
+let featureTitle = document.querySelectorAll('.text-content h4')
+
+let featureContent = document.querySelectorAll('.text-content p')
+
+console.log(featureTitle)
+
+headersArr = ["features-h4","about-h4", "services-h4", "product-h4", "vision-h4" ]
+contentArr = ["features-content", "about-content", "services-content", "product-content","vision-content"]
+i = 0
+featureTitle.forEach(element => {
+  element.textContent = siteContent["main-content"][headersArr[i]]
+  i++
+})
+i = 0
+featureContent.forEach(element => {
+  element.textContent = siteContent["main-content"][contentArr[i]]
+  i++
+})
+
+let middleImg = document.querySelector('.middle-img')
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+let contactHeader = document.querySelector('.contact h4')
+contactHeader.textContent = siteContent["contact"]["contact-h4"]
+
+let contactContent = document.querySelectorAll('.contact p')
+console.log(contactContent)
+contactArr = ["address","phone", "email" ]
+i = 0
+contactContent.forEach(element => {
+  element.textContent = siteContent["contact"][contactArr[i]]
+  i++
+})
+
+let footer = document.querySelector('footer p')
+footer.textContent = siteContent["footer"]["copyright"]
+/**
+ * ## Task 3: Add new content
+* [ ] Change the color of the navigation text to be green.
+* [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
+* [ ] Check your work by looking at the [original html](original.html) in the browser
+ */
+nav.forEach(element => {
+  element.style.color = "green"
+})
+
+let appendNode = document.createElement("a")
+appendNode.textContent = "Learn More"
+appendNode.style.color = "green"
+
+let prependNode = document.createElement("a")
+prependNode.textContent = "Learn More"
+prependNode.style.color = "green"
+
+nav[0].appendChild(appendNode)
+nav[nav.length-1].appendChild(prependNode)
